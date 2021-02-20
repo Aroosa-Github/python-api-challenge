90b884759f5c82e79d980a3f6347297c0b631bb5
Aroosa-Github <aroosa@outlook.com>
2021-02-20 12:37:45 +0800

    initial

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+# weather_api_key = "675a8ffed32f35b4ce6f2fc49f3fa24c"
+weather_api_key = "AIzaSyA4lJFyBgPf7KOHyoNe5YGZrpBXsH-qlzs"
+
+# Google API Key"AIzaSyC6mtqLvGbWE6lhkoDBKoMymDfcl_fZ1Sw
+
+g_key = "AIzaSyAjtf5E39_GnP6DPEq5zzwvqxxNYE3EEgs"
+
+
